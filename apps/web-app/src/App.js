583bf4f3e90885d6b679246ea46462df08df9044
Villem Alango <valango@users.noreply.github.com>
2026-04-19 19:03:48 +0300

    fixed: api-server name

--- apps/web-app/src/App.js
+++ apps/web-app/src/App.js
@@ -1,24 +1,28 @@
+import { add } from '@monorepo/utils'; // import here
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const result = add(2, 3);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Addition of 2 and 3 is <h1> {result}</h1>
+          </p>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
   );
 }
 
